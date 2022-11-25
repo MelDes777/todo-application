fc9439931d279408f6f7b4ef48e010502c576eb8
Strakatov Nikolay <desux327@gmail.com>
2022-11-26 01:14:35 +0600

    +1 button

--- todo-application/Models/Todo.cs
+++ todo-application/Models/Todo.cs
@@ -7,12 +7,28 @@ namespace todo_application.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "You must enter a Description!")]
+        [MinLength(3, ErrorMessage = "Title must contain at least two characters!")]
+        [MaxLength(200, ErrorMessage = "Title must contain a maximum of 200 characters!")]
         public string Description { get; set; }
+
+        
+        [Required(ErrorMessage = "You must pick a Date!")]
+        [Display(Name = "Date Created")]
         public DateTime DateCreated { get; set; }
+
+        
+        [Required(ErrorMessage = "You must pick a Due Date!")]
+        [Display(Name = "Due Date")]
         public DateTime DueDate { get; set; }
+
+        [Display(Name = "Completed")]
         public bool IsCompleted { get; set; }
+
+        [Display(Name = "Started")]
         public bool IsStarted { get; set; }
+
+        [Display(Name = "In Progress")]
         public bool IsProgressed { get; set; }
 
     }
---
+++ todo-application/Models/TodoListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace todo_application.Models
+{
+    public class TodoListViewModel
+    {
+     
+        public List<Todo> TodoItems { get; set; }
+
+        public Todo EditableItem { get; set; }
+    }
+}
