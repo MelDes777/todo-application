6212f9a5bca36908d0edde169b6841412361f12e
Strakatov Nikolay <desux327@gmail.com>
2022-11-26 13:29:04 +0600

    o

--- todo-app-tests/TodoList-Tests.cs
+++ todo-app-tests/TodoList-Tests.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using todo_application.Infrastructure;
+using todo_application.Models;
 using NUnit.Framework;
+using System.Runtime.InteropServices;
 
 namespace todo_app_tests
 {
@@ -9,10 +19,55 @@ namespace todo_app_tests
         {
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+        //[Test] 
+        //public async Task<IActionResult> Index_GetDb_ToView()
+        //{
+
+        //}
+
+        //[Test]
+        //public async Task<IActionResult> Details(int? id)
+        //{
+            
+        //}
+
+        //[Test]
+        //public IActionResult Create()
+        //{
+            
+        //}
+
+        //[Test]
+        //public async Task<IActionResult> Create([Bind("Id,Description,DateCreated," +
+        //    "DueDate,IsCompleted,IsStarted,IsProgressed")] Todo todo)
+        //{
+            
+        //}
+
+        //[Test]
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+            
+        //}
+
+        //[Test]
+        //public async Task<IActionResult> Edit(int id,
+        //    [Bind("Id,Description,DateCreated,DueDate,IsCompleted,IsStarted,IsProgressed")] Todo todo)
+        //{
+            
+        //}
+
+        //[Test]
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+            
+        //}
+
+        //[Test]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+           
+        //}
+
     }
 }
