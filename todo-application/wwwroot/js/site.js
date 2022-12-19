b07c68a025dd2904323615faa7c29f42a064af9e
Strakatov Nikolay <desux327@gmail.com>
2022-12-19 16:42:00 +0600

    Almost 2

--- todo-application/wwwroot/js/site.js
+++ todo-application/wwwroot/js/site.js
@@ -2,8 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
-//$(function myFunction1() {
-//    var completed = $('.myClass').hide(3000).show(3000);
-//});
